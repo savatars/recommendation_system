663affeaebd0528aedb7ebfe6526bd1f88052dbe
anand-371 <anandvardhan045@gmail.com>
2020-03-24 17:07:49 +0530

    updated_file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
